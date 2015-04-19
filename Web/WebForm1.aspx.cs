7ed5c32141559a54d9ac94d684e4fe49ab1312d4
glebars <k72kpdp>
2015-04-20 10:18:08 +1200

    Send an email

---
+++ Web/WebForm1.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnstuff.Click += (s, args) => {
+                var melClient = new SmtpClient();
+                melClient.Send("Tygogoz", "glebars@jadeworld.com; glebars@gmail.com", "Bienvenue tygogoz", "contenu de test");
+            };
+        }
+    }
+}
